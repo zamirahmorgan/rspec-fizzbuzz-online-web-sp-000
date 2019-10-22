d40ce3c3b5bdcef227674df69a29ca620f1a1912
clean-form-8028 <zamirahmo66@gmail.com>
2019-10-22 02:35:00 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,15 +3,15 @@
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go Fizz
-  end
+end
 def fizzbuzz(int)
     if int % 5 == 0 # if the number int is divisible by 5
       "Buzz" # Go Buzz
-    end
-    def fizzbuzz(int)
-      if int % 15 == 0 # if the number int is divisible by 3
-        "FizzBuzz" # Go FizzBuzz
-      end
+end
+def fizzbuzz(int)
+    if int % 15 == 0 # if the number int is divisible by 3
+    "FizzBuzz" # Go FizzBuzz
+ end
 end
 
 fizzbuzz(3) # You should see a return of "Fizz"
