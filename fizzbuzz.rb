8837cca75651d3628010cf95d732dccc88a041fe
clean-form-8028 <zamirahmo66@gmail.com>
2019-10-22 02:36:21 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,23 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(int)
+  if int % 3 == 0 # if the number int is divisible by 3
+    "Fizz" # Go Fizz
+end
+def fizzbuzz(int)
+    if int % 5 == 0 # if the number int is divisible by 5
+      "Buzz" # Go Buzz
+end
+def fizzbuzz(int)
+    if int % 15 == 0 # if the number int is divisible by 3
+    "FizzBuzz" # Go FizzBuzz
+ end
+ def fizzbuzz(int)
+     if int % !4 == 0 # if the number int is divisible by 3
+     "FizzBuzz" # Go FizzBuzz
+  end
+end
+
+fizzbuzz(3) # You should see a return of "Fizz"
+fizzbuzz(5) # You should see a return of nil
+fizzbuzz() # You should get an ArgumentError
